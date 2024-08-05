bda683fe8fe8bc224ef41387a63e6cca1f6e5aab
Abdelrhman <mrsheko234@gmail.com>
2024-08-05 22:40:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currentOperand, setCurrentOperand] = useState('');
+  const [previousOperand, setPreviousOperand] = useState('');
+  const [operation, setOperation] = useState('');
+
+  const appendNumber = (number) => {
+    if (number === '.' && currentOperand.includes('.')) return;
+    setCurrentOperand(currentOperand + number);
+  };
+
+  const chooseOperation = (operation) => {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+      compute();
+    }
+    setOperation(operation);
+    setPreviousOperand(currentOperand);
+    setCurrentOperand('');
+  };
+
+  const compute = () => {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    setCurrentOperand(computation.toString());
+    setOperation('');
+    setPreviousOperand('');
+  };
+
+  const clear = () => {
+    setCurrentOperand('');
+    setPreviousOperand('');
+    setOperation('');
+  };
+
+  const deleteLast = () => {
+    setCurrentOperand(currentOperand.toString().slice(0, -1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className='output'>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
+      </div>
+
+      <button className='span-two' onClick={clear}>AC</button>
+      <button onClick={deleteLast}>DEL</button>
+      <button onClick={() => chooseOperation('/')}>/</button>
+      <button onClick={() => appendNumber('1')}>1</button>
+      <button onClick={() => appendNumber('2')}>2</button>
+      <button onClick={() => appendNumber('3')}>3</button>
+      <button onClick={() => chooseOperation('*')}>*</button>
+      <button onClick={() => appendNumber('4')}>4</button>
+      <button onClick={() => appendNumber('5')}>5</button>
+      <button onClick={() => appendNumber('6')}>6</button>
+      <button onClick={() => chooseOperation('+')}>+</button>
+      <button onClick={() => appendNumber('7')}>7</button>
+      <button onClick={() => appendNumber('8')}>8</button>
+      <button onClick={() => appendNumber('9')}>9</button>
+      <button onClick={() => chooseOperation('-')}>-</button>
+      <button onClick={() => appendNumber('.')}>.</button>
+      <button onClick={() => appendNumber('0')}>0</button>
+      <button className='span-two' onClick={compute}>=</button>
     </div>
   );
 }
